1dd279f832f092e04bc64f346b83a7dc3aeb4487
Kool2rock <kuldeep.110059@gmail.com>
2021-08-15 14:08:39 +0530

    Update perly.pl

--- perly.pl
+++ perly.pl
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use perl1;
 use Data::Dumper;
-my $Body = '{“FirstName”: “Kumar””LastName”: “value”,”UserName”: “Kuldeep”,”Password”: “Password123”,”Email”: “Kuldeep\@kumar.com”}';
+my $Body = '{“FirstName”: “Kumar””LastName”: “value”,”UserName”: “Kuldeep”,”Password”: “Password123”,”Email”: “Kuldeep@kumar.com”}';
 my $obj = new POSTReq($Body,"http://restapi.demoqa.com/customer/register");
 my $obj1 = new DELETEReq("http:// restapi.demoqa.com/customer/delete/15410");
 $obj->POST_Req;
